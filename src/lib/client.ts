79029feac62215e2e8003456c12a28452a48d4c0
lawal <sphade010@gmail.com>
2024-11-26 16:51:52 +0100

    refactor: Remove baseURL from authClient configuration

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,6 +1,5 @@
 import { adminClient } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/svelte'; // make sure to import from better-auth/svelte
 export const authClient = createAuthClient({
-	baseURL: 'http://localhost:5173',
 	plugins: [adminClient()]
 });
